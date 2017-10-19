d8ceae60a7ce6d335d3411c23d27e29ad3f862ad
Brandon Lee <brandonlee0405@yahoo.com>
2017-10-19 11:50:28 -0700

    Added onClick to drop down the yellow.png drawable

--- app/src/main/java/com/example/brandon/connectthree/MainActivity.java
+++ app/src/main/java/com/example/brandon/connectthree/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.brandon.connectthree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1000f).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test
     }
 }
