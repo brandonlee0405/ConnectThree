46f68e903398d191266198190f393e6e885fe11b
Brandon Lee <brandonlee0405@yahoo.com>
2017-10-25 14:21:31 -0700

    If there is a winner, game ends and play again pops up

--- app/src/main/java/com/example/brandon/connectthree/MainActivity.java
+++ app/src/main/java/com/example/brandon/connectthree/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -49,6 +50,9 @@ public class MainActivity extends AppCompatActivity
                 {
 
                     System.out.println(gameState[winningPosition[0]]);
+
+                    LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(view.VISIBLE);
                 }
             }
 
